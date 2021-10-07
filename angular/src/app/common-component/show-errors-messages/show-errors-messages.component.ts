a8019ad2093f2a6f1e10f0f57557c4b9f8be5d62
Francisco Javier Rosa <pacorosa99@gmail.com>
2021-10-08 00:03:03 +0200

    Actualizacion

--- angular/src/app/common-component/show-errors-messages/show-errors-messages.component.ts
+++ angular/src/app/common-component/show-errors-messages/show-errors-messages.component.ts
@@ -3,19 +3,41 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'errors-messages',
   templateUrl: './show-errors-messages.component.html',
-  styleUrls: ['./show-errors-messages.component.scss']
+  styleUrls: ['./show-errors-messages.component.scss'],
 })
 export class ShowErrorsMessagesComponent implements OnInit {
-
   @Input() errors: any;
+  required: boolean = false;
+  minlength: string = '';
 
-  object(){
-   return JSON.stringify(this.errors);
+  object() {
+    if(this.errors !== null){
+    for (let key of Object.keys(this.errors)) {
+      console.log(key)
+      switch (key) {
+        case 'required':
+          return 'Campo requerido'
+        case 'minlength':
+          return `El campo debe ser mayor a ${this.errors[key]["requiredLength"]} caracteres`
+          case 'maxlength':
+            return `El campo debe ser menor a ${this.errors[key]["requiredLength"]} caracteres`
+        default:
+          return '';
+      }
+    }
+  }
+  return ''
+}
+
+  init(value: any){
+
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  json() {
+    return JSON.stringify(this.errors);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
