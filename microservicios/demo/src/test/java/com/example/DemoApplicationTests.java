f8bc3417e6b0b01735a52edbcddf8a7e6faaa8f3
frarosrod2 <pacorosa99@gmail.com>
2021-10-20 16:17:40 +0200

    Configuración jpa

--- microservicios/demo/src/test/java/com/example/DemoApplicationTests.java
+++ microservicios/demo/src/test/java/com/example/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.ioc.Servicio;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	Servicio srv;
+	
 	@Test
-	void contextLoads() {
+	void inyecciones() {
+		assertEquals("Soy una taza", srv.saluda());
 	}
 
 }
