87132febeb799013889a739288594bff16cc6126
frarosrod2 <pacorosa99@gmail.com>
2021-10-31 12:18:29 +0100

    Avance catalogo

--- catalogo/src/app/catalogo/catalogo.component.ts
+++ catalogo/src/app/catalogo/catalogo.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CatalogoViewModelService } from './catalogo.service';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
+  providers: [CatalogoViewModelService],
   styleUrls: ['./catalogo.component.scss']
 })
-export class CatalogoComponent implements OnInit {
+export class CatalogoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
+  constructor(protected vm: CatalogoViewModelService, protected route: ActivatedRoute) { }
+  public get VM(): CatalogoViewModelService { return this.vm; }
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    if (id) {
+      if (this.route.snapshot.url.slice(-1)[0]?.path === 'edit') {
+        this.vm.edit(+id);
+      } else {
+        this.vm.view(+id);
+      }
+    } else if (this.route.snapshot.url.slice(-1)[0]?.path === 'add') {
+      this.vm.add();
+    } else {
+      this.vm.load();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.vm.clear()
   }
 
 }
+
+
+export const
+CATALOGOS_COMPONENTES = [CatalogoComponent]
--- catalogo/src/app/catalogo/catalogo.module.ts
+++ catalogo/src/app/catalogo/catalogo.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CatalogoComponent } from '.';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonComponentModule } from '../common-component';
+import { MyCoreModule } from 'src/lib/my-core';
+import { CommonServicesModule } from '../common-services';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CatalogoComponent],
+  exports:[CatalogoComponent],
   imports: [
-    CommonModule
+    CommonModule, FormsModule, RouterModule.forChild([]),
+    MyCoreModule, CommonComponentModule, MyCoreModule, CommonServicesModule
   ]
 })
 export class CatalogoModule { }
